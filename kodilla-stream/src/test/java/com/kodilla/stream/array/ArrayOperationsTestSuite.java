8d11e489c3714e614dcd08989a910f41b215e58d
lukkra1989 <luk.kramarczyk@gmail.com>
2020-10-24 14:41:13 +0200

    7.5 Intstream ArrayOperation

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream.array;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArrayOperationsTestSuite implements ArrayOperations {
+
+    @Test
+    public void testGetAverage() {
+        int[] figures = new int[]{
+                1, 2, 3, 4, 5, 7, 88, 99, 1102, 11, 55, 22, 78, 90, 34, 879, 9, 5, 1
+        };
+        final double result = ArrayOperations.getAverage(figures);
+        assertEquals(131.31578947368422, result);
+    }
+
+}
