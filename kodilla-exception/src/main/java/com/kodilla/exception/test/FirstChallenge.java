0a4dd04dca787c849b428656a3bb771786d4f09a
lukkra1989 <luk.kramarczyk@gmail.com>
2020-10-27 10:16:49 +0100

    8.2 FirstChallange

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -1,29 +1,31 @@
 package com.kodilla.exception.test;
 
-import java.nio.file.Files;
-import java.util.stream.Stream;
-
 public class FirstChallenge {
 
-    public double divide(double a, double b) throws ArithmeticException{
-        try {
-            divide(3,0);
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
         }
-        catch (ArithmeticException e) {
-            System.out.println("Błąd dzielenia przez 0 !!!");
-    }finally{ return  a/b;
-        }
+        return a / b;
     }
 
     /**
      * This main can throw an ArithmeticException!!!
+     *
      * @param args
      */
-    public static void  main (String[]args){
-        FirstChallenge firstChallenge=new FirstChallenge();
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
 
-        double result = firstChallenge.divide(3,0);
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Błąd dzielenia przez 0!");
+        } finally {
+            System.out.println("Wywołanie finally");
+        }
 
-        System.out.println(result);
+
     }
 }
