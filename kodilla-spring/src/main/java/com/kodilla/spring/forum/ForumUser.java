cc028ac79b3f2be7635b9ec35fec45df8c070fe6
lukkra1989 <luk.kramarczyk@gmail.com>
2020-11-19 19:55:32 +0100

    14.1

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -1,11 +1,19 @@
 package com.kodilla.spring.forum;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ForumUser {
+
     String username = "John Smith";
 
+
+    public ForumUser() {
+        this.username = username;
+    }
+
+    @Bean
     public String getUsername() {
         return username;
     }
--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -1,14 +1,18 @@
 package com.kodilla.spring.forum;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 public class ForumUserTestSuite {
+
     @Test
     void TestGetUsername() {
+        //Given
         ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
         ForumUser name = context.getBean(ForumUser.class);
         //When
@@ -16,4 +20,5 @@ public class ForumUserTestSuite {
         //Then
         assertEquals("John Smith", userName);
     }
+
 }
