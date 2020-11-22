f964621b4125eb1d454cef164463afbf3a190ddb
lukkra1989 <luk.kramarczyk@gmail.com>
2020-11-22 12:10:31 +0100

    15.1 Singleton

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,18 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class LoggerTestSuite {
+    @Test
+    void shouldGetLastLog(){
+        Logger logger=Logger.getInstance();
+        logger.log("Example Text");
+
+        String result = logger.getLastLog();
+
+        assertEquals(result,"Example Text");
+    }
+}
