5ab929f2935dcedf8d617abfab10c965b3003df5
lukkra1989 <luk.kramarczyk@gmail.com>
2020-11-22 18:36:49 +0100

    15.1 Singleton

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/singleton/Logger.java
@@ -0,0 +1,23 @@
+package com.kodilla.patterns.singleton;
+
+public class Logger {
+
+    private static final Logger INSTANCE=new Logger();
+    private String lastLog = "";
+
+    private Logger() {
+    }
+
+    public static Logger getInstance(){
+        return INSTANCE;
+    }
+
+    public void log(String log) {
+        lastLog = log;
+        System.out.println("Log: [" + log + "]");
+    }
+
+    public String getLastLog() {
+        return lastLog;
+    }
+}
--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class LoggerTestSuite {
+
     @Test
     void shouldGetLastLog(){
         Logger logger=Logger.getInstance();
