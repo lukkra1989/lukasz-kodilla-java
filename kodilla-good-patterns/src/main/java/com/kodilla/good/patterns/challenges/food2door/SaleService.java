2a5ef0ba248a2c61acabb74932d32b1a7acd87b5
lukkra1989 <luk.kramarczyk@gmail.com>
2020-11-30 09:19:19 +0100

    15.2 Prototype

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door/SaleService.java
+++
@@ -1,8 +0,0 @@
-package com.kodilla.good.patterns.challenges.food2door;
-
-public class OrderGenerator {
-    public boolean createOrder(){
-//        Order order= new Order(createOrder().getOrderName(),createOrder().getOrderAmmount());
-        return  true;
-    }
-}
--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -13,50 +13,30 @@ public class LibraryTestSuite {
         //Given
         Library myLibrary = new Library("Fantasy");
         for (int i = 0; i < 5; i++) {
-            myLibrary.getBooks().add(new Book("title1" , "Writer X" , LocalDate.of(1990, 1, 2)));
+            myLibrary.getBooks().add(new Book("title1", "Writer X", LocalDate.of(1990, 1, 2)));
         }
 
-        System.out.println(myLibrary);
-
         Book booksByY = new Book("Title2 ", "Writer Y", LocalDate.of(1980, 2, 3));
-        System.out.println(myLibrary.getBooks().size());
         myLibrary.getBooks().remove(booksByY);
-        System.out.println(myLibrary.getBooks().size());
 
         //making a shallow copy
         Library clonedLibrary = null;
         try {
             clonedLibrary = myLibrary.shallowCopy();
-            clonedLibrary.setName("Novels");
-            //myLibrary.getBooks().add(new Book())
         } catch (CloneNotSupportedException e) {
             System.out.println(e);
         }
-        System.out.println(myLibrary);
-        System.out.println(clonedLibrary);
 
         //making a deep copy
         Library deepClonedLibrary = null;
         try {
             deepClonedLibrary = myLibrary.deepCopy();
-            deepClonedLibrary.setName("Horrors");
         } catch (CloneNotSupportedException e) {
             System.out.println(e);
         }
 
-        //When
-        myLibrary.getBooks().remove("Fantasy");
-
-        //Then
-        System.out.println(myLibrary);
-        System.out.println(clonedLibrary);
-        System.out.println(deepClonedLibrary);
-        assertEquals(5, myLibrary.getBooks().size());
-        assertEquals(5, clonedLibrary.getBooks().size());
-        assertEquals(5, deepClonedLibrary.getBooks().size());
-        assertEquals(clonedLibrary.getBooks(), myLibrary.getBooks());
-        assertEquals(deepClonedLibrary.getBooks(), myLibrary.getBooks());
-
+        //When&Then
+        assertEquals(clonedLibrary, myLibrary);
+        assertEquals(deepClonedLibrary, myLibrary);
     }
-
 }
