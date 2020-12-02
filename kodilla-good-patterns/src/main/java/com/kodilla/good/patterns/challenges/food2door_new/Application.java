2d7ec99226952fdde2615dac88604afbcec6a94a
lukkra1989 <luk.kramarczyk@gmail.com>
2020-12-02 13:16:12 +0100

    13.4 New food2door

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door_new/Application.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door_new/Application.java
@@ -4,12 +4,16 @@ import java.util.Arrays;
 
 public class
 Application {
+
     public static void main(String[] args) throws ShopNotFoundException {
+
         OrderService orderService = new OrderService();
+        orderService.checkingProducerMap();
 
             orderService.orderFromShop("ExtraFoodShop", Arrays.asList(new Product("Chocolate", 5, 10)));
             orderService.orderFromShop("GlutenFreeShop", Arrays.asList(new Product("Milk", 2, 12)));
             orderService.orderFromShop("HealthyShop", Arrays.asList(new Product("Orange", 12, 4)));
+
 
     }
 }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door_new/OrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/food2door_new/OrderService.java
@@ -5,20 +5,28 @@ import java.util.List;
 import java.util.Map;
 
 public class OrderService {
+        final Map<String, Producer> producerMap = new HashMap<>();
+        {
+            producerMap.put("ExtraFoodShop", new ExtraFoodShop());
+            producerMap.put("GlutenFreeShop", new GlutenFreeShop());
+            producerMap.put("HealthyShop", new HealthyShop());
+        }
 
-    final Map<String,Producer>producerMap=new HashMap<>();
-    {
-        producerMap.put("ExtraFoodShop",new ExtraFoodShop());
-        producerMap.put("GlutenFreeShop",new GlutenFreeShop());
-        producerMap.put("HealthyShop",new HealthyShop());
+    void checkingProducerMap(ExtraFoodShop extraFoodShop, GlutenFreeShop glutenFreeShop, HealthyShop healthyShop)throws ShopNotFoundException{
+        if (!producerMap.containsKey((extraFoodShop))) {
+            throw new ShopNotFoundException();
+        }
+        if (!producerMap.containsKey((glutenFreeShop))) {
+            throw new ShopNotFoundException();
+        }
+        if (!producerMap.containsKey((healthyShop))) {
+            throw new ShopNotFoundException();
+        }
     }
-    void orderFromShop(String shopName, List<Product>products) throws ShopNotFoundException{
+
+    void orderFromShop(String shopName, List<Product> products) {
         System.out.println("Start Ordering Products");
         producerMap.get(shopName).process(products);
         System.out.println("Finishing Ordering Products");
-
-        if(products.contains(producerMap)){
-            throw new ShopNotFoundException();
-        }
     }
 }
