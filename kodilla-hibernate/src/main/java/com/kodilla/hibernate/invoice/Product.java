cf5aa30d00c6a848d7153759f8fd7bf01ec1a030
lukkra1989 <luk.kramarczyk@gmail.com>
2020-12-09 12:54:47 +0100

    17.3 InvoiceDao

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/invoice/Product.java
@@ -0,0 +1,120 @@
+package com.kodilla.hibernate.invoice;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "PRODUCT")
+public class Product {
+    private int id;
+    private String name;
+    private List<Item> items = new ArrayList<>();
+
+    public Product() {
+    }
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "NAME")
+    public String getName() {
+        return name;
+    }
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(
+            targetEntity = Item.class,
+            mappedBy = "product",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+}
+
+
+
+//@Entity
+//@Table(name = "PRODUCTS")
+//public class Product {
+//    private int id;
+//    private String name;
+//    private Item item;
+//    private List<Item> items = new ArrayList<>();
+//
+//    public Product() {
+//    }
+//
+//    public Product(String name) {
+//        this.name = name;
+//    }
+//
+//    @Id
+//    @NotNull
+//    @GeneratedValue
+//    @Column(name = "ID", unique = true)
+//    public int getId() {
+//        return id;
+//    }
+//
+//    @NotNull
+//    @Column(name = "PRODUCTNAME")
+//    public String getName() {
+//        return name;
+//    }
+//
+//    @OneToMany(
+//            targetEntity = Item.class,
+//           mappedBy = "product",
+//            cascade = CascadeType.ALL,
+//            fetch = FetchType.LAZY
+//    )
+//    public List<Item> getItem() {
+//        return items;
+//    }
+//
+//
+//    public Item getItem() {
+//        return item;
+//    }
+//
+//    public List<Item> getItems() {
+//        return items;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setItem(Item item) {
+//        this.item = item;
+//    }
+//}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,62 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Iterator;
+
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+
+    @Autowired
+    InvoiceDao invoiceDao;
+    @Test
+    void testInvoiceDaoSave(){
+
+        //Given
+        Product product1=new Product("X");
+        Product product2=new Product("Y");
+
+        Item item1=new Item();
+        Item item2=new Item();
+
+        Invoice invoice1=new Invoice("One");
+        invoice1.getItems().add(item1);
+
+        Invoice invoice2=new Invoice("Two");
+        invoice2.getItems().add(item2);
+
+        //When
+       // invoiceDao.save();
+        invoiceDao.save(invoice1);
+        invoiceDao.save(invoice2);
+
+        int id= invoice1.getId();
+        int id2= invoice2.getId();
+
+        //Then
+        Assert.assertNotEquals(0,id);
+        Assert.assertNotEquals(0,id2);
+
+        //CleanUp
+        try{
+            invoiceDao.deleteById(id);
+            invoiceDao.deleteById(id2);
+        }catch (Exception e){
+            //do nothing
+        }
+
+
+
+
+//
+//        invoiceDao.deleteById(id);
+//        invoiceDao.deleteById(id2);
+
+    }
+}
