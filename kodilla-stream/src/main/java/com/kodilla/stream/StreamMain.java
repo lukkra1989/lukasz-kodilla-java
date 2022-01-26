a88a96d22d6bf2c640a3abbebdc3b608f445a330
lukkra1989 <l.kramarczyk@o2.pl>
2022-01-26 10:01:07 +0100

    Creating 7.5

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -4,31 +4,31 @@ import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.forumuser.Forum;
 import com.kodilla.stream.forumuser.ForumUser;
 
-import java.sql.SQLOutput;
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
-       /* System.out.println("Task 7.1");
-        PoemBeautifier poemBeautifier=new PoemBeautifier();
-
-        System.out.println("Getting Poem more beautiful with lambdas");
-        System.out.println();
-
-        poemBeautifier.beautify("Lithuania ,Motherland Ov Mine !",
-                (myPoem) ->"ABC" +" " + myPoem + " "+"ABC" );
-
-        poemBeautifier.beautify("Lithuania ,Motherland Ov Mine !",
-                (myPoem) -> myPoem.toUpperCase());
-
-        poemBeautifier.beautify("Lithuania ,Motherland Ov Mine !",
-                ((myPoem) -> myPoem.toLowerCase()));
-
-        poemBeautifier.beautify("Lithuania ,Motherland Ov Mine !",
-                ((myPoem) -> String.valueOf(myPoem.getBytes())));*/
+//        System.out.println("Task 7.1");
+//        PoemBeautifier poemBeautifier = new PoemBeautifier();
+//
+//        System.out.println("Getting Poem more beautiful with lambdas");
+//        System.out.println();
+//
+//        poemBeautifier.beautify("Lithuania ,Motherland Ov Mine !",
+//                (myPoem) -> "ABC" + " " + myPoem + " " + "ABC");
+//
+//        poemBeautifier.beautify("Lithuania ,Motherland Ov Mine !",
+//                (myPoem) -> myPoem.toUpperCase());
+//
+//        poemBeautifier.beautify("Lithuania ,Motherland Ov Mine !",
+//                ((myPoem) -> myPoem.toLowerCase()));
+//
+//        poemBeautifier.beautify("Lithuania ,Motherland Ov Mine !",
+//                ((myPoem) -> String.valueOf(myPoem.getBytes())));
 
         System.out.println();
         System.out.println("Task 7.3:");
@@ -38,8 +38,8 @@ public class StreamMain {
                 .filter(user -> user.getSex() == 'M')
                 .filter(user -> ChronoUnit.YEARS.between(user.getDateOfBirth(), LocalDate.now()) > 19)
                 .filter(user -> user.getPost() > 0)
-                .collect(Collectors.toMap(user -> user.getUserId(), user -> user));
-        // .collect(Collectors.toMap(ForumUser::getUserId, Function.identity()));
+                //.collect(Collectors.toMap(user -> user.getUserId(), user -> user));
+        .collect(Collectors.toMap(ForumUser::getUserId, Function.identity()));
         System.out.println(collect);
 
     }
--- kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/Forum.java
@@ -5,12 +5,15 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Forum {
+
     public static List<ForumUser> getUserList() {
+        ForumUser user5 = new ForumUser(5, "Walter", 'M', LocalDate.of(1970, 2, 1), 2);
         final List<ForumUser> theUserList = new LinkedList<>();
-        theUserList.add(new ForumUser(22, "John", 'M', LocalDate.of(1990, 3, 2), 1));
-        theUserList.add(new ForumUser(16, "Emma", 'F', LocalDate.of(1970, 5, 12), 155));
-        theUserList.add(new ForumUser(01, "Tony", 'M', LocalDate.of(1980, 12, 9), 16));
-        theUserList.add(new ForumUser(100, "Larysa", 'F', LocalDate.of(2001, 3, 7), 201));
+        theUserList.add(new ForumUser(1, "John", 'M', LocalDate.of(1990, 3, 2), 1));
+        theUserList.add(new ForumUser(2, "Emma", 'F', LocalDate.of(1970, 5, 12), 155));
+        theUserList.add(new ForumUser(3, "Tony", 'M', LocalDate.of(1980, 12, 9), 16));
+        theUserList.add(new ForumUser(4, "Larysa", 'F', LocalDate.of(2001, 3, 7), 201));
+        theUserList.add(user5);
         return theUserList;
     }
 }
